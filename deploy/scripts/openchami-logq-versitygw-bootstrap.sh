c2693c3e9588e91de4fabe6668e01090a046e62c
Sean Tronsen <sean.tronsen@gmail.com>
2026-02-22 21:29:11 -0700

    fix bucket permissions for log-compactor
    Signed-off-by: Sean Tronsen <sean.tronsen@gmail.com>

--- deploy/scripts/openchami-logq-versitygw-bootstrap.sh
+++ deploy/scripts/openchami-logq-versitygw-bootstrap.sh
@@ -211,6 +211,7 @@ aws --profile "${ROOT_PROFILE}" \
     s3api put-bucket-acl \
     --bucket "${BUCKET_RAW}" \
     --grant-write "$(get_user_access ${USER_LOG_WRITER})" \
+    --grant-write "$(get_user_access ${USER_LOG_COMPACTOR})" \
     --grant-read "$(get_user_access ${USER_LOG_WRITER})" \
     --grant-read "$(get_user_access ${USER_LOG_COMPACTOR})"
 
@@ -253,6 +254,12 @@ PATH_WORK=$(mktemp -d)
       "Effect": "Allow",
       "Principal": "$(get_user_access ${USER_LOG_COMPACTOR})",
       "Action": ["s3:GetObject"],
+      "Resource": ["arn:aws:s3:::${BUCKET_RAW}/*"]
+    },
+    {
+      "Effect": "Allow",
+      "Principal": "$(get_user_access ${USER_LOG_COMPACTOR})",
+      "Action": ["s3:DeleteObject"],
       "Resource": ["arn:aws:s3:::${BUCKET_RAW}/*"]
     },
     {
