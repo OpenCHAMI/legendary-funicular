815fd3adc5d1e45e4953865d7ce4bc622f10e7da
Sean Tronsen <sean.tronsen@gmail.com>
2026-03-03 01:55:49 -0700

    prepare dynamic sources generation from global opts
    Signed-off-by: Sean Tronsen <sean.tronsen@gmail.com>

--- query/cmd/opts/opts.go
+++ query/cmd/opts/opts.go
@@ -2,9 +2,12 @@
 package opts
 
 import (
+	"fmt"
 	"io"
 	"os"
+	"slices"
 
+	"github.com/seantronsen/openchami-logq/query/internal/config"
 	"github.com/seantronsen/openchami-logq/query/internal/utils"
 	"github.com/spf13/cobra"
 )
@@ -12,9 +15,14 @@ import (
 type Opts struct {
 	Format string
 	Output io.Writer
+	Source string
 }
 
 func FromCobraCmd(cmd *cobra.Command) Opts {
+
+	source, err := cmd.Root().PersistentFlags().GetString("source")
+	utils.CheckFatal(err)
+
 	format, err := cmd.Root().PersistentFlags().GetString("format")
 	utils.CheckFatal(err)
 
@@ -28,5 +36,19 @@ func FromCobraCmd(cmd *cobra.Command) Opts {
 		w = f
 	}
 
-	return Opts{Format: format, Output: w}
+	return Opts{Format: format, Output: w, Source: source}
+}
+
+func (o *Opts) BuildSources(cfg *config.Config) []string {
+	// todo: once the temp compaction for raw ndjson records is complete,
+	// add the location(s) here as well.
+	validSources := []string{"logs", "events"}
+
+	var sources []string
+
+	if !slices.Contains(validSources, o.Source) {
+		utils.CheckFatal(fmt.Errorf("detected invalid source: '%s'", o.Source))
+	}
+	sources = append(sources, fmt.Sprintf("s3://%s/%s/**/*.parquet", *cfg.S3BucketParquet, o.Source))
+	return sources
 }
--- query/cmd/root.go
+++ query/cmd/root.go
@@ -22,6 +22,7 @@ DuckDB. It supports ad-hoc SQL queries, predefined reports, and dataset
 inspection using serverless technologies.`,
 	}
 
+	rootCmd.PersistentFlags().StringP("source", "s", "logs", "output file path (logs, events)")
 	rootCmd.PersistentFlags().StringP("format", "f", "ndjson", "output format (json, ndjson)")
 	rootCmd.PersistentFlags().StringP("output", "o", "", "output file path (default: stdout)")
 
