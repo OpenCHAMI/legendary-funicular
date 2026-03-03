f38645c51422ebf4946d0be1ed7d0893ceedff0b
Sean Tronsen <sean.tronsen@gmail.com>
2026-03-03 01:58:50 -0700

    add previously missing utils file
    Signed-off-by: Sean Tronsen <sean.tronsen@gmail.com>

---
+++ query/internal/utils/utils.go
@@ -0,0 +1,29 @@
+// Package utils
+package utils
+
+import (
+	"fmt"
+	"log/slog"
+	"os"
+)
+
+func CheckFatal(err error) {
+	if err != nil {
+		slog.Error(fmt.Sprint(err))
+		os.Exit(1)
+	}
+}
+
+func GetEnv(name string) (string, error) {
+	value := os.Getenv(name)
+	if value == "" {
+		return value, fmt.Errorf("undefined required environment variable: '%s'", name)
+	}
+	return value, nil
+}
+
+func GetEnvFatal(name string) string {
+	value, err := GetEnv(name)
+	CheckFatal(err)
+	return value
+}
