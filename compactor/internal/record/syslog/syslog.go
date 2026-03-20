f03ea2c5d3630117a88c36820593bdc9715e1e14
Sean Tronsen <sean.tronsen@gmail.com>
2026-03-20 16:23:03 -0600

    reconfigure compactor to use the standardized colnames
    Signed-off-by: Sean Tronsen <sean.tronsen@gmail.com>

--- compactor/internal/record/syslog/syslog.go
+++ compactor/internal/record/syslog/syslog.go
@@ -7,21 +7,27 @@ package syslog
 
 import "encoding/json"
 
+// todo: revisit and reconsider if all fields should be pointers
+
 type Syslog struct {
-	Timestamp   *string `json:"timestamp" parquet:"ts"`
-	Service     *string `json:"appname" parquet:"service"`
-	Host        *string `json:"host" parquet:"host"`
-	Severity    *string `json:"severity" parquet:"level"`
-	Message     *string `json:"message" parquet:"msg"`
-	XName       *string `json:"xname" parquet:"xname"`
-	IDComponent *string `json:"component_id" parquet:"component_id"`
-	IDNode      *string `json:"node_id" parquet:"node_id"`
-	IDTrace     *string `json:"trace_id" parquet:"trace_id"`
-	IDRequest   *string `json:"request_id" parquet:"request_id"`
-	RequestURI  *string `json:"request_uri" parquet:"request_uri"`
-	RequestUser *string `json:"request_user" parquet:"request_user"`
-	ParseError  *string `json:"parse_error" parquet:"parse_error"`
-	PayloadJSON *string `json:"payload_json" parquet:"payload_json"`
+	// CORE
+	Timestamp   *string         `json:"ts" parquet:"ts"`
+	Service     *string         `json:"service" parquet:"service"`
+	Host        *string         `json:"host" parquet:"host"`
+	Level       *string         `json:"level" parquet:"level"`
+	Message     *string         `json:"msg" parquet:"msg"`
+	Data        *string          `json:"data" parquet:"data"`
+	// OPENCHAMI SPECIFIC
+	XName       *string         `json:"xname" parquet:"xname"`
+	IDComponent *string         `json:"component_id" parquet:"component_id"`
+	IDNode      *string         `json:"node_id" parquet:"node_id"`
+	IDTrace     *string         `json:"trace_id" parquet:"trace_id"`
+	IDRequest   *string         `json:"request_id" parquet:"request_id"`
+	RequestURI  *string         `json:"request_uri" parquet:"request_uri"`
+	RequestUser *string         `json:"request_user" parquet:"request_user"`
+	// PARSING
+	ParseError  *string         `json:"parse_error" parquet:"parse_error"`
+	PayloadJSON *string         `json:"payload_json" parquet:"payload_json"`
 }
 
 func Parse(b []byte) (Syslog, error) {
