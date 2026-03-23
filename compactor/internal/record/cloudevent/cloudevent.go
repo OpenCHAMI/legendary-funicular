1aea86ea5277be271725f342c585aee49380daee
Sean Tronsen <sean.tronsen@gmail.com>
2026-03-23 12:49:59 -0600

    revise compactor schema for cloud events
    Signed-off-by: Sean Tronsen <sean.tronsen@gmail.com>

--- compactor/internal/record/cloudevent/cloudevent.go
+++ compactor/internal/record/cloudevent/cloudevent.go
@@ -7,29 +7,41 @@ package cloudevent
 
 import "encoding/json"
 
-type Event struct {
-	ID          *string         `json:"id" parquet:"id"`
-	Source      *string         `json:"source" parquet:"source"`
-	Timestamp   *string         `json:"time" parquet:"ts"`
-	SpecVersion *string         `json:"specversion" parquet:"specversion"`
-	Type        *string         `json:"type" parquet:"type"`
-	DataRaw     json.RawMessage `json:"data" parquet:"-"`
-	Data        string          `json:"-" parquet:"data"`
-}
-
 type CloudEvent struct {
-	Host        *string `json:"host" parquet:"host"`
+	// Core
+	Timestamp *string `json:"ts" parquet:"ts"`
+
+	TransportMethod   *string `json:"transport_method" parquet:"transport_method"`
+	TransportMetadata *string `json:"transport_metadata" parquet:"transport_metadata"`
+
+	Event            *string `json:"cloudevent" parquet:"cloudevent"`
+	EventID          *string `json:"cloudevent_id" parquet:"cloudevent_id"`
+	EventSource      *string `json:"cloudevent_source" parquet:"cloudevent_source"`
+	EventType        *string `json:"cloudevent_type" parquet:"cloudevent_type"`
+	EventSpecversion *string `json:"cloudevent_specversion" parquet:"cloudevent_specversion"`
+	EventBinding     *string `json:"cloudevent_binding" parquet:"cloudevent_binding"`
+
+	// OPENCHAMI SPECIFIC
+	XName       *string `json:"xname" parquet:"xname"`
+	IDComponent *string `json:"component_id" parquet:"component_id"`
+	IDNode      *string `json:"node_id" parquet:"node_id"`
+	IDTrace     *string `json:"trace_id" parquet:"trace_id"`
+	IDRequest   *string `json:"request_id" parquet:"request_id"`
+	RequestURI  *string `json:"request_uri" parquet:"request_uri"`
+	RequestUser *string `json:"request_user" parquet:"request_user"`
+
+	// PARSING
+	Raw         *string `json:"raw" parquet:"raw"` // raw as operated on by datadog vector
 	ParseError  *string `json:"parse_error" parquet:"parse_error"`
-	PayloadJSON string  `json:"payload_json" parquet:"payload_json"`
-	Event       Event   `json:"cloudevent" parquet:"cloudevent"`
+	PayloadJSON *string `json:"payload_json" parquet:"payload_json"`
 }
 
 func Parse(b []byte) (CloudEvent, error) {
 	var record CloudEvent
 	err := json.Unmarshal(b, &record)
 	if err == nil {
-		record.PayloadJSON = string(b)
-		record.Event.Data = string(record.Event.DataRaw)
+		jsonStr := string(b)
+		record.PayloadJSON = &jsonStr
 	}
 	return record, err
 }
